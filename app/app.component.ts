647d4bd178c508369898369cbee2af90d0cc2f48
Timur Karpenok <nbvehbq@gmail.com>
2017-06-29 22:43:15 +0300

    data bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  {
+    title: 'Learn JavaScript',
+    completed: true,
+  },
+  {
+    title: 'Learn Angular 2',
+    completed: false,
+  },
+  {
+    title: 'Write application',
+    completed: false,
+  },
+];
+
 @Component({
   moduleId: module.id,
   selector: 'app',
@@ -8,4 +23,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular 2 Do';
+  todos = todos;
+
+  toggle(todo: any) {
+    console.log('togle', todo);
+    todo.completed = !todo.completed;
+  }
+
+  delete(todo: any) {
+    let index = this.todos.indexOf(todo);
+
+    if (index > -1) {
+      this.todos.splice(index, 1);
+    }
+  }
 }
